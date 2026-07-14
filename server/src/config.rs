5f8e31120b583749ff75142bbf284b965c2016a1
Cieron <cirrow@proton.me>
2026-07-15 10:34:26 +1200

    rename config

--- server/src/config.rs
+++ server/src/config.rs
@@ -18,7 +18,7 @@ pub struct Basic {
     pub image_path: Option<String>,
     pub pinned_pages: Vec<String>,
     pub dev_url: Option<String>,
-    pub url_directory_prefix: Option<String>,
+    pub wikipage_directory_prefix: Option<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
