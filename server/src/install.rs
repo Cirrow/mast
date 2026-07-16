b315a15eb357ff94d9c61e83ffef02c88b6dd4e7
Cieron <cirrow@proton.me>
2026-07-17 00:26:55 +1200

    config generator

--- server/src/install.rs
+++ server/src/install.rs
@@ -1,5 +1,7 @@
 use crate::config::CFG;
 use crate::pages::inject;
+use axum::response::{Html, Redirect};
+use std::collections::HashSet;
 use std::fs;
 
 struct InstallRequest {
@@ -24,8 +26,57 @@ pub fn mast_initialised() -> bool {
     }
 }
 
-pub async fn serve_install() -> Html<String> {
+pub async fn serve_install_page() -> Html<String> {
+    if mast_initialised() {}
     let path = CFG.base_dir.join("src/install.html");
     let content = std::fs::read_to_string(&path).unwrap_or_default();
-    Html(inject(content))
+    Html(inject(&content))
+}
+
+pub async fn generate_config(mut req: InstallRequest) {
+    let mut base_user_permissions = Hashset::new();
+    let mut auth_methods: Vec<String>;
+
+    match req.init_acl_policy {
+        "Open" => {
+            base_user_permissions.addAll(List.of("r", "u", "c"));
+            auth_methods = vec!["username"];
+        }
+        "Public" => {
+            edit_requires_auth = true;
+            auth_methods = vec!["username"];
+        }
+        "Private" => {
+            edit_requires_auth = true;
+            auth_methods = vec![];
+        }
+    }
+
+    let data = String::from(format!(
+        r#"
+        # ----- WIKI SETUP -----
+        [basic]
+        name = "{req.name}"
+        wikipage_directory_prefix = "/wiki/"
+        image_as_home = false
+        pinned_pages = [""]
+
+        [auth]
+        allow_signup = {req.allow_signup}
+        edit_requires_auth = {}
+        users_file = "conf/users.toml"
+        auth_methods = ["username"] # supported: ["github", "gitlab", "google", "apple", "facebook"]
+
+        [storage]
+        type = "local_git"
+        location = ".wiki/wiki"
+
+        # ----- WIKI CUSTOMISATION -----
+        [shell]
+        shell = "default"
+
+
+
+        "#
+    ));
 }
